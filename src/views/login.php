9cebf3e8b104738da301a7f7a515582004ac8ef3
Anfisa <midlovetsanfisa@gmail.com>
2024-11-02 18:00:15 +0200

    бу

--- src/views/login.php
+++ src/views/login.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Andev web - Перевірка форми</title>
     <link rel="stylesheet" href="/public/css/style.css" />
     <link
         rel="stylesheet"
